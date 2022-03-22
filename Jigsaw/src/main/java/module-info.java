697bc5bf7afbedd52c939220f1f8cafa0d4a446a
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2022-03-22 23:01:51 +0300

    project configured

---
+++ Jigsaw/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module bse.sda.jigsaw {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens bse202.sda.jigsaw to javafx.fxml;
+    exports bse202.sda.jigsaw;
+    exports bse202.sda.jigsaw.controllers;
+    opens bse202.sda.jigsaw.controllers to javafx.fxml;
+}
