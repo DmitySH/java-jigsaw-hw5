1d927ab10324aa8d51c04b686d7373d33a059474
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2022-03-27 22:38:33 +0300

    all figures added

--- Jigsaw/src/main/java/bse202/sda/jigsaw/models/fxml/Figure.java
+++ Jigsaw/src/main/java/bse202/sda/jigsaw/models/fxml/Figure.java
@@ -17,9 +17,36 @@ import java.util.Optional;
 public class Figure extends Group implements CoordinateTransfer {
     public enum FigureType {
         FIRST,
+        FIRST90,
+        FIRST180,
+        FIRST270,
+        FIRST_REFLECTED,
+        FIRST_REFLECTED90,
+        FIRST_REFLECTED180,
+        FIRST_REFLECTED270,
         SECOND,
+        SECOND_REFLECTED,
+        SECOND90,
+        SECOND_REFLECTED90,
         THIRD,
+        THIRD90,
+        THIRD180,
+        THIRD270,
         FOURTH,
+        FOURTH90,
+        FOURTH180,
+        FOURTH270,
+        FIFTH,
+        FIFTH90,
+        SIXTH,
+        SEVENTH,
+        SEVENTH90,
+        SEVENTH180,
+        SEVENTH270,
+        EIGHTH,
+        EIGHTH90,
+        EIGHTH180,
+        EIGHTH270,
     }
 
     public static Rotater2D rotater;
@@ -41,25 +68,179 @@ public class Figure extends Group implements CoordinateTransfer {
         coordinates = new HashMap<>();
 
         switch (type) {
-            case FIRST -> figureFirst();
-            case SECOND -> {
-                figureFirst();
+            case FIRST -> figureFirst(0);
+            case FIRST90 -> {
+                figureFirst(0);
                 setRotate(90);
                 for (IntPoint point :
                         coordinates.values()) {
                     rotater.rotate(90, point);
                 }
             }
-            case THIRD -> {
-                figureFirst();
+            case FIRST180 -> {
+                figureFirst(0);
                 setRotate(180);
                 for (IntPoint point :
                         coordinates.values()) {
                     rotater.rotate(180, point);
                 }
             }
-            case FOURTH -> {
-                figureFirst();
+            case FIRST270 -> {
+                figureFirst(0);
+                setRotate(270);
+                for (IntPoint point :
+                        coordinates.values()) {
+                    rotater.rotate(270, point);
+                }
+            }
+            case FIRST_REFLECTED -> figureFirst(1);
+            case FIRST_REFLECTED90 -> {
+                figureFirst(1);
+                setRotate(90);
+                for (IntPoint point :
+                        coordinates.values()) {
+                    rotater.rotate(90, point);
+                }
+            }
+            case FIRST_REFLECTED180 -> {
+                figureFirst(1);
+                setRotate(180);
+                for (IntPoint point :
+                        coordinates.values()) {
+                    rotater.rotate(180, point);
+                }
+            }
+            case FIRST_REFLECTED270 -> {
+                figureFirst(1);
+                setRotate(270);
+                for (IntPoint point :
+                        coordinates.values()) {
+                    rotater.rotate(270, point);
+                }
+            }
+            case SECOND -> figureSecond(0);
+            case SECOND_REFLECTED -> figureSecond(1);
+
+            case SECOND90 -> {
+                figureSecond(0);
+                setRotate(90);
+                for (IntPoint point :
+                        coordinates.values()) {
+                    rotater.rotate(90, point);
+                }
+            }
+            case SECOND_REFLECTED90 -> {
+                figureSecond(1);
+                setRotate(90);
+                for (IntPoint point :
+                        coordinates.values()) {
+                    rotater.rotate(90, point);
+                }
+            }
+            case THIRD -> figureThird(2);
+            case THIRD90 -> {
+                figureThird(2);
+                setRotate(90);
+                for (IntPoint point :
+                        coordinates.values()) {
+                    rotater.rotate(90, point);
+                }
+            }
+            case THIRD180 -> {
+                figureThird(2);
+                setRotate(180);
+                for (IntPoint point :
+                        coordinates.values()) {
+                    rotater.rotate(180, point);
+                }
+            }
+            case THIRD270 -> {
+                figureThird(2);
+                setRotate(270);
+                for (IntPoint point :
+                        coordinates.values()) {
+                    rotater.rotate(270, point);
+                }
+            }
+            case FOURTH -> figureThird(1);
+            case FOURTH90 -> {
+                figureThird(1);
+                setRotate(90);
+                for (IntPoint point :
+                        coordinates.values()) {
+                    rotater.rotate(90, point);
+                }
+            }
+            case FOURTH180 -> {
+                figureThird(1);
+                setRotate(180);
+                for (IntPoint point :
+                        coordinates.values()) {
+                    rotater.rotate(180, point);
+                }
+            }
+            case FOURTH270 -> {
+                figureThird(1);
+                setRotate(270);
+                for (IntPoint point :
+                        coordinates.values()) {
+                    rotater.rotate(270, point);
+                }
+            }
+            case FIFTH -> figureFifth();
+            case FIFTH90 -> {
+                figureFifth();
+                setRotate(90);
+                for (IntPoint point :
+                        coordinates.values()) {
+                    rotater.rotate(90, point);
+                }
+            }
+            case SIXTH -> figureSixth();
+            case SEVENTH -> figureSeventh();
+            case SEVENTH90 -> {
+                figureSeventh();
+                setRotate(90);
+                for (IntPoint point :
+                        coordinates.values()) {
+                    rotater.rotate(90, point);
+                }
+            }
+            case SEVENTH180 -> {
+                figureSeventh();
+                setRotate(180);
+                for (IntPoint point :
+                        coordinates.values()) {
+                    rotater.rotate(180, point);
+                }
+            }
+            case SEVENTH270 -> {
+                figureSeventh();
+                setRotate(270);
+                for (IntPoint point :
+                        coordinates.values()) {
+                    rotater.rotate(270, point);
+                }
+            }
+            case EIGHTH -> figureEighth();
+            case EIGHTH90 -> {
+                figureEighth();
+                setRotate(90);
+                for (IntPoint point :
+                        coordinates.values()) {
+                    rotater.rotate(90, point);
+                }
+            }
+            case EIGHTH180 -> {
+                figureEighth();
+                setRotate(180);
+                for (IntPoint point :
+                        coordinates.values()) {
+                    rotater.rotate(180, point);
+                }
+            }
+            case EIGHTH270 -> {
+                figureEighth();
                 setRotate(270);
                 for (IntPoint point :
                         coordinates.values()) {
@@ -69,19 +250,112 @@ public class Figure extends Group implements CoordinateTransfer {
         }
     }
 
-    private void figureFirst() {
-        for (int i = 0; i < 3; i++) {
+    private void figureFirst(int offset) {
+        for (int i = 0; i < 2; i++) {
             Rectangle r = createRectangle();
+            r.setX(rectSize * i);
+            coordinates.put(r, new IntPoint((int) r.getX() / rectSize,
+                    (int) r.getY() / rectSize));
+            this.getChildren().add(r);
+        }
+
+        for (int i = 1; i < 3; i++) {
+            Rectangle r = createRectangle();
+            r.setX(rectSize * offset);
             r.setY(rectSize * i);
             coordinates.put(r, new IntPoint((int) r.getX() / rectSize,
                     (int) r.getY() / rectSize));
             this.getChildren().add(r);
         }
+    }
+
+    private void figureSecond(int offset) {
+        for (int i = 0; i < 2; i++) {
+            Rectangle r = createRectangle();
+            r.setX(offset * rectSize);
+            r.setY(rectSize * i);
+            coordinates.put(r, new IntPoint((int) r.getX() / rectSize,
+                    (int) r.getY() / rectSize));
+            this.getChildren().add(r);
+        }
+
+        for (int i = 1; i < 3; i++) {
+            Rectangle r = createRectangle();
+            r.setX(rectSize - offset * rectSize);
+            r.setY(rectSize * i);
+            coordinates.put(r, new IntPoint((int) r.getX() / rectSize,
+                    (int) r.getY() / rectSize));
+            this.getChildren().add(r);
+        }
+    }
+
+    private void figureThird(int offset) {
+        for (int i = 0; i < 3; i++) {
+            Rectangle r = createRectangle();
+            r.setX(i * rectSize);
+            r.setY(rectSize * 2);
+            coordinates.put(r, new IntPoint((int) r.getX() / rectSize,
+                    (int) r.getY() / rectSize));
+            this.getChildren().add(r);
+        }
+
+        for (int i = 0; i < 2; i++) {
+            Rectangle r = createRectangle();
+            r.setX(offset * rectSize);
+            r.setY(rectSize * i);
+            coordinates.put(r, new IntPoint((int) r.getX() / rectSize,
+                    (int) r.getY() / rectSize));
+            this.getChildren().add(r);
+        }
+    }
+
+    private void figureFifth() {
+        for (int i = 0; i < 3; i++) {
+            Rectangle r = createRectangle();
+            r.setX(i * rectSize);
+            coordinates.put(r, new IntPoint((int) r.getX() / rectSize,
+                    (int) r.getY() / rectSize));
+            this.getChildren().add(r);
+        }
+    }
+
+    private void figureSixth() {
         Rectangle r = createRectangle();
+        coordinates.put(r, new IntPoint((int) r.getX() / rectSize,
+                (int) r.getY() / rectSize));
+        this.getChildren().add(r);
+    }
+
+    private void figureSeventh() {
+        for (int i = 0; i < 2; i++) {
+            Rectangle r = createRectangle();
+            r.setX(i * rectSize);
+            coordinates.put(r, new IntPoint((int) r.getX() / rectSize,
+                    (int) r.getY() / rectSize));
+            this.getChildren().add(r);
+        }
+
+        Rectangle r = createRectangle();
+        r.setY(rectSize);
+        coordinates.put(r, new IntPoint((int) r.getX() / rectSize,
+                (int) r.getY() / rectSize));
+        this.getChildren().add(r);
+    }
+
+    private void figureEighth() {
+        for (int i = 0; i < 3; i++) {
+            Rectangle r = createRectangle();
+            r.setY(i * rectSize);
+            coordinates.put(r, new IntPoint((int) r.getX() / rectSize,
+                    (int) r.getY() / rectSize));
+            this.getChildren().add(r);
+        }
+
+        Rectangle r = createRectangle();
+        r.setY(rectSize);
         r.setX(rectSize);
         coordinates.put(r, new IntPoint((int) r.getX() / rectSize,
                 (int) r.getY() / rectSize));
-
         this.getChildren().add(r);
     }
 
