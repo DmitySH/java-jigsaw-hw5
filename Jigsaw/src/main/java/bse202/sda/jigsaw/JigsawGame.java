d87e788680460faffdb9ae3b633cb711790b1721
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2022-03-28 11:54:04 +0300

    second window added

--- Jigsaw/src/main/java/bse202/sda/jigsaw/JigsawGame.java
+++ Jigsaw/src/main/java/bse202/sda/jigsaw/JigsawGame.java
@@ -6,18 +6,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class JigsawGame extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(
-                JigsawGame.class.getResource("views/game.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1240, 980);
-        scene.getStylesheets().add(Objects.requireNonNull(JigsawGame.class
-                .getResource("views/css/styles.css")).toExternalForm());
-        stage.setTitle("Jigsaw By DmitriySH");
-        stage.setScene(scene);
+                JigsawGame.class.getResource("views/main.fxml"));
+
+        Scene mainScene = new Scene(fxmlLoader.load(), 340, 580);
+
+
+        stage.setTitle("Main");
+        stage.setScene(mainScene);
         stage.show();
     }
 
---
+++ Jigsaw/src/main/java/bse202/sda/jigsaw/controllers/MainController.java
@@ -0,0 +1,47 @@
+package bse202.sda.jigsaw.controllers;
+
+import bse202.sda.jigsaw.JigsawGame;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+    @FXML
+    public Button startButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void createGameWindow(ActionEvent e) {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                JigsawGame.class.getResource("views/game.fxml"));
+        Stage newWindow = new Stage();
+
+        try {
+            Scene gameScene = new Scene(fxmlLoader.load(), 1240, 980);
+            gameScene.getStylesheets().add(Objects.requireNonNull(JigsawGame.class
+                    .getResource("views/css/styles.css")).toExternalForm());
+
+            newWindow.initModality(Modality.WINDOW_MODAL);
+
+            newWindow.initOwner(startButton.getScene().getWindow());
+            newWindow.setTitle("Jigsaw By DmitriySH");
+            newWindow.setScene(gameScene);
+            newWindow.show();
+        } catch (IOException ex) {
+            System.out.println("Unable to create game window");
+        }
+    }
+}
